3ff1b3714526caec2c7f903dd8347ca4d164400d
Joni Sirviö <jonisirvio@Jonis-Laptop.local>
2024-11-05 16:41:57 +0200

    Lisätty binomikertoimen laskeminen

--- esim_3/main.cpp
+++ esim_3/main.cpp
@@ -5,15 +5,23 @@ using namespace std;
 int main() {
     int num;
     int result;
-    cout << "Lasketaan kertoma!" << endl;
-    cout << "Anna positiivinen kokonaisluku" << endl;
-    cin >> num;
-    result = factorial(num);
-    if (result == -1) {
-        cout << "Kertomaa ei voi laskea negatiiviselle luvulle " << endl;
-    } else {
-        cout << "Luvun " << num << " kertoma = " << result << endl;
-    }
+    int n, k;
+//    cout << "Lasketaan kertoma!" << endl;
+//    cout << "Anna positiivinen kokonaisluku" << endl;
+//    cin >> num;
+//    result = factorial(num);
+//    if (result == -1) {
+//        cout << "Kertomaa ei voi laskea negatiiviselle luvulle " << endl;
+//    } else {
+//        cout << "Luvun " << num << " kertoma = " << result << endl;
+//    }
+
+    cout<<"Anna n"<<endl;
+    cin>>n;
+    cout<<"Anna k"<<endl;
+    cin>>k;
+    result=coefficient(n,k);
+    cout<<n<<" yli "<<k<<" = "<<result<<endl;
 
     return 0;
 }
--- esim_3/myfunctions.cpp
+++ esim_3/myfunctions.cpp
@@ -13,3 +13,15 @@ int factorial(int n)
     }
 }
 
+
+int coefficient(int n, int k)
+{
+    if (k>n || k<0) {
+        return 0;
+    }
+    else{
+        int result;
+        result= factorial(n) / (factorial(k)*factorial(n-k));
+        return result;
+    }
+}
